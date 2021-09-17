83ad0e0ba834a29735d952014e38792115e0ea70
danielsp <daniel.s3000@gmail.com>
2021-09-17 16:03:44 +0200

    Modify sdk min version checks

---
+++ sdk/src/main/java/it/ministerodellasalute/verificaC19sdk/VerificaMinVersionException.kt
@@ -0,0 +1,8 @@
+package it.ministerodellasalute.verificaC19sdk
+
+class VerificaMinVersionException : Exception {
+    constructor() : super()
+    constructor(message: String) : super(message)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+    constructor(cause: Throwable) : super(cause)
+}
