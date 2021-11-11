cc8eb39e1f23587226faf1d2252b083af7e79fe9
Nicola MC <nicolamariacornelio01@gmail.com>
2021-11-11 11:05:41 +0100

    feature: handle status client errors

--- sdk/src/main/java/it/ministerodellasalute/verificaC19sdk/data/VerifierRepositoryImpl.kt
+++ sdk/src/main/java/it/ministerodellasalute/verificaC19sdk/data/VerifierRepositoryImpl.kt
@@ -233,36 +233,50 @@ class VerifierRepositoryImpl @Inject constructor(
     }
 
     private suspend fun getCRLStatus() {
-        val response = apiService.getCRLStatus(preferences.currentVersion)
-        if (response.isSuccessful) {
-            crlstatus = Gson().fromJson(response.body()?.string(), CrlStatus::class.java)
-            Log.i("CRL Status", crlstatus.toString())
+        try {
+            val response = apiService.getCRLStatus(preferences.currentVersion)
+            if (response.isSuccessful) {
+                crlstatus = Gson().fromJson(response.body()?.string(), CrlStatus::class.java)
+                Log.i("CRL Status", crlstatus.toString())
 
-            crlstatus?.let { crlStatus ->
-                if (isRetryAllowed()) {
-                    if (outDatedVersion(crlStatus)) {
-                        if (noPendingDownload() || preferences.authorizedToDownload == 1L) {
-                            saveCrlStatusInfo(crlStatus)
-                            if (isSizeOverThreshold(crlStatus) && preferences.authorizedToDownload == 0L && !preferences.shouldInitDownload) {
-                                preferences.isSizeOverThreshold = true
-                            } else {
-                                preferences.shouldInitDownload = false
-                                downloadChunks()
+                crlstatus?.let { crlStatus ->
+                    if (isRetryAllowed()) {
+                        if (outDatedVersion(crlStatus)) {
+                            if (noPendingDownload() || preferences.authorizedToDownload == 1L) {
+                                saveCrlStatusInfo(crlStatus)
+                                if (isSizeOverThreshold(crlStatus) && preferences.authorizedToDownload == 0L && !preferences.shouldInitDownload) {
+                                    preferences.isSizeOverThreshold = true
+                                } else {
+                                    preferences.shouldInitDownload = false
+                                    downloadChunks()
+                                }
+                            } else if (preferences.authToResume == 1L) {
+                                if (isSameChunkSize(crlStatus) && sameRequestedVersion(crlStatus)) downloadChunks()
+                                else {
+                                    clearDBAndPrefs()
+                                    this.syncData(context)
+                                }
                             }
-                        } else if (preferences.authToResume == 1L) {
-                            if (isSameChunkSize(crlStatus) && sameRequestedVersion(crlStatus)) downloadChunks()
-                            else {
-                                clearDBAndPrefs()
-                                this.syncData(context)
-                            }
+                        } else {
+                            manageFinalReconciliation()
                         }
                     } else {
-                        manageFinalReconciliation()
+                        maxRetryReached.postValue(true)
                     }
-                } else {
-                    maxRetryReached.postValue(true)
-                }
 
+                }
+            } else {
+                throw HttpException(response)
+            }
+        }catch (e: HttpException) {
+            if (e.code() in 400..407) {
+                Log.i(e.toString(), e.message())
+                currentRetryNum++
+                clearDBAndPrefs()
+                preferences.shouldInitDownload = true
+                this.syncData(context)
+            } else {
+                Log.i("StatusHttpException: $e", e.message())
             }
         }
     }
@@ -408,7 +422,7 @@ class VerifierRepositoryImpl @Inject constructor(
                         this.syncData(context)
                         break
                     } else {
-                        Log.i("HttpException: $e", e.message())
+                        Log.i("ChunkHttpException: $e", e.message())
                         break
                     }
                 } catch (e: CancellationException) {
