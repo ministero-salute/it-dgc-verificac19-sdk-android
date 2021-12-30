ccc50018a3f131b0751db507b9514b723268fec6
Light2288 <davide.aliti@gmail.com>
2021-12-30 13:17:03 +0100

    develop-networkmanager
    remove isOnline function

--- sdk/src/main/java/it/ministerodellasalute/verificaC19sdk/util/Utility.kt
+++ sdk/src/main/java/it/ministerodellasalute/verificaC19sdk/util/Utility.kt
@@ -96,24 +96,4 @@ object Utility {
             .digest(input.toByteArray()))
     }
 
-    fun isOnline(context: Context): Boolean {
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val capabilities =
-            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-        if (capabilities != null) {
-            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
-                Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
-                return true
-            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-                Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
-                return true
-            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
-                Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
-                return true
-            }
-        }
-        return false
-    }
-
 }
